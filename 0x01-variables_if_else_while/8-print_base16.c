2cd4d9a599fff0df14ebe14693fad0097f3cf59a
Your Name <you@example.com>
2023-02-19 15:56:50 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
  */
 
 int main(void)
-{
-	char c;
-        
-	int d;
+{ 
+        char c;
+    
+        int d;
 
-	c = 'a';
-        d = 0;
-	while
-		(d < 10) {
-			putchar(d + '0');
-	         }        d++;
-       while
-	        (c <= 'f') {
-			putchar(c);
-                        c++;
-		}
+        c = 'a';
+        while
+	       (d < 10) {
+	               putchar(d + '0');
+	               d++;
+	       }
+        while
+	       (c <= 'f') {
+	              putchar(c);
+                      c++;
+               }
         putchar('\n');
-     	return (0);
-}
-
+        return (0);
+}	
